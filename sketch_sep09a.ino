e00becff8c19ee5b560a198c43b67fd116736fb6
mviel <mathieuviel00@gmail.com>
2020-10-09 15:05:46 -0500

    Proto control esp depuis une page web

--- sketch_sep09a.ino
+++ sketch_sep09a.ino
@@ -6,24 +6,61 @@ const char* ssid = "test";
 const char* password = "cegenredemdp";
 WebServer server(80);
 
+const int led = 2;
+bool etatLed = 0;
+char texteEtatLed[2][10] = {"ÉTEINTE!","ALLUMÉE!"};
+
 void handleRoot()
 {
     String page = "<!DOCTYPE html>";
+
+    page += "<html lang='fr'>";
+
     page += "<head>";
     page += "    <title>Serveur ESP32</title>";
-    page += "    <meta http-equiv='refresh' content='60' name='viewport' content='width=device-width, initial-scale=1' charset='UTF-8'/>";
+    page += "    <meta http-equiv='refresh' content='60' name='viewport' content='width=device-width, initial-scale=1' charset='UTF-8' />";
+    page += "    <link rel='stylesheet' href='https://www.w3schools.com/w3css/4/w3.css'>";
     page += "</head>";
 
-    page += "<body lang='fr'>";
-    page += "    <h1>Serveur</h1>";
-    page += "    <p>Ce serveur est hébergé sur un ESP32</p>";
-    page += "    <b>Créé par Mathieu VIEL</b>";
+    page += "<body>";
+    page += "    <div class='w3-card w3-blue w3-padding-small w3-jumbo w3-center'>";
+    page += "        <p>ÉTAT LED: "; page += texteEtatLed[etatLed]; + "</p>";
+    page += "    </div>";
+
+    page += "    <div class='w3-bar'>";
+    page += "        <a href='/on' class='w3-bar-item w3-button w3-border w3-jumbo' style='width:50%; height:50%;'>ON</a>";
+    page += "        <a href='/off' class='w3-bar-item w3-button w3-border w3-jumbo' style='width:50%; height:50%;'>OFF</a>";
+    page += "    </div>";
+
+    page += "    <div class='w3-center w3-padding-16'>";
+    page += "        <p>Ce serveur est hébergé sur un ESP32</p>";
+    page += "        <i>Créé par Mathieu VIEL</i>";
+    page += "    </div>";
+
     page += "</body>";
 
     page += "</html>";
 
+    server.setContentLength(page.length());
     server.send(200, "text/html", page);
 }
+
+void handleOn()
+{
+    etatLed = 1;
+    digitalWrite(led, HIGH);
+    server.sendHeader("Location","/");
+    server.send(303);
+}
+
+void handleOff()
+{
+    etatLed = 0;
+    digitalWrite(led, LOW);
+    server.sendHeader("Location","/");
+    server.send(303);
+}
+
 void handleNotFound()
 {
     server.send(404, "text/plain", "404: Not found");
@@ -36,6 +73,9 @@ void setup()
   Serial.begin(115200);
   delay(1000);
   Serial.println("\n");
+
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW);
   
   if(!wm.autoConnect(ssid, password))
     Serial.println("Erreur de connexion.");
@@ -43,6 +83,8 @@ void setup()
     Serial.println("Connexion etablie!");
 
   server.on("/", handleRoot);
+  server.on("/on", handleOn);
+  server.on("/off", handleOff);
   server.onNotFound(handleNotFound);
   server.begin();
 
