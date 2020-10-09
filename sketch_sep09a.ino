bf4de89c35cb18810b013fc61bf25efa48bff5cd
mviel <mathieuviel00@gmail.com>
2020-10-09 14:49:04 -0500

    Proto WifiManager avec page web

--- sketch_sep09a.ino
+++ sketch_sep09a.ino
@@ -1,8 +1,33 @@
 #include <WiFiManager.h>
+#include <WebServer.h>
 
 WiFiManager wm;
 const char* ssid = "test";
 const char* password = "cegenredemdp";
+WebServer server(80);
+
+void handleRoot()
+{
+    String page = "<!DOCTYPE html>";
+    page += "<head>";
+    page += "    <title>Serveur ESP32</title>";
+    page += "    <meta http-equiv='refresh' content='60' name='viewport' content='width=device-width, initial-scale=1' charset='UTF-8'/>";
+    page += "</head>";
+
+    page += "<body lang='fr'>";
+    page += "    <h1>Serveur</h1>";
+    page += "    <p>Ce serveur est hébergé sur un ESP32</p>";
+    page += "    <b>Créé par Mathieu VIEL</b>";
+    page += "</body>";
+
+    page += "</html>";
+
+    server.send(200, "text/html", page);
+}
+void handleNotFound()
+{
+    server.send(404, "text/plain", "404: Not found");
+}
 
 void setup()
 {
@@ -16,6 +41,12 @@ void setup()
     Serial.println("Erreur de connexion.");
   else
     Serial.println("Connexion etablie!");
+
+  server.on("/", handleRoot);
+  server.onNotFound(handleNotFound);
+  server.begin();
+
+  Serial.println("Serveur web actif!");
 }
 
 void loop()
@@ -27,4 +58,5 @@ void loop()
     wm.resetSettings();
     ESP.restart();
   }
+  server.handleClient();
 }
