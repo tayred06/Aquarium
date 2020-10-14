e01e529e5bbdb5fd2a763aeb26fd2401d2daac04
mviel <mathieuviel00@gmail.com>
2020-10-14 14:27:32 -0500

    PDC oled

--- sketch_sep09a.ino
+++ sketch_sep09a.ino
@@ -1,104 +1,61 @@
-#include <WiFiManager.h>
-#include <WebServer.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-WiFiManager wm;
-const char* ssid = "test";
-const char* password = "cegenredemdp";
-WebServer server(80);
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
-const int led = 2;
-bool etatLed = 0;
-char texteEtatLed[2][10] = {"ÉTEINTE!","ALLUMÉE!"};
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-void handleRoot()
-{
-    String page = "<!DOCTYPE html>";
+void setup() {
+  Serial.begin(9600);
 
-    page += "<html lang='fr'>";
-
-    page += "<head>";
-    page += "    <title>Serveur ESP32</title>";
-    page += "    <meta http-equiv='refresh' content='60' name='viewport' content='width=device-width, initial-scale=1' charset='UTF-8' />";
-    page += "    <link rel='stylesheet' href='https://www.w3schools.com/w3css/4/w3.css'>";
-    page += "</head>";
-
-    page += "<body>";
-    page += "    <div class='w3-card w3-blue w3-padding-small w3-jumbo w3-center'>";
-    page += "        <p>ÉTAT LED: "; page += texteEtatLed[etatLed]; + "</p>";
-    page += "    </div>";
-
-    page += "    <div class='w3-bar'>";
-    page += "        <a href='/on' class='w3-bar-item w3-button w3-border w3-jumbo' style='width:50%; height:50%;'>ON</a>";
-    page += "        <a href='/off' class='w3-bar-item w3-button w3-border w3-jumbo' style='width:50%; height:50%;'>OFF</a>";
-    page += "    </div>";
-
-    page += "    <div class='w3-center w3-padding-16'>";
-    page += "        <p>Ce serveur est hébergé sur un ESP32</p>";
-    page += "        <i>Créé par Mathieu VIEL</i>";
-    page += "    </div>";
-
-    page += "</body>";
-
-    page += "</html>";
-
-    server.setContentLength(page.length());
-    server.send(200, "text/html", page);
-}
-
-void handleOn()
-{
-    etatLed = 1;
-    digitalWrite(led, HIGH);
-    server.sendHeader("Location","/");
-    server.send(303);
-}
-
-void handleOff()
-{
-    etatLed = 0;
-    digitalWrite(led, LOW);
-    server.sendHeader("Location","/");
-    server.send(303);
-}
-
-void handleNotFound()
-{
-    server.send(404, "text/plain", "404: Not found");
-}
-
-void setup()
-{
-  WiFi.mode(WIFI_STA);
-  
-  Serial.begin(115200);
-  delay(1000);
-  Serial.println("\n");
-
-  pinMode(led, OUTPUT);
-  digitalWrite(led, LOW);
-  
-  if(!wm.autoConnect(ssid, password))
-    Serial.println("Erreur de connexion.");
-  else
-    Serial.println("Connexion etablie!");
-
-  server.on("/", handleRoot);
-  server.on("/on", handleOn);
-  server.on("/off", handleOff);
-  server.onNotFound(handleNotFound);
-  server.begin();
-
-  Serial.println("Serveur web actif!");
-}
-
-void loop()
-{
-  //Dans cet exemple j'utilise la broche tactile D4 pour faire un reset des paramètres de connexion.
-  if(touchRead(T0) < 50)
-  {
-    Serial.println("Suppression des reglages et redemarrage...");
-    wm.resetSettings();
-    ESP.restart();
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
   }
-  server.handleClient();
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+
+  // Draw a single pixel in white
+  display.drawPixel(10, 10, SSD1306_WHITE);
+
+  // Show the display buffer on the screen. You MUST call display() after
+  // drawing commands to make them visible on screen!
+  display.display();
+  delay(2000);
+
+
+  testdrawchar();      // Draw characters of the default font
+
+  Serial.println(F("Fin du setup"));
+
+}
+
+void loop() {
+}
+
+void testdrawchar(void) {
+  display.clearDisplay();
+
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setCursor(1, 0);     // Start at top-left corner
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+
+  display.write("Adresse ip :\r\n 192.168.0.4 \r\n");
+  display.write("Temperature :\r\n 25 deg \r\n");
+  display.write("Prochaine activation de la pompe :\r\n 2 heures \r\n");
+
+  display.display();
+  delay(2000);
 }
