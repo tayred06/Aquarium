eb07ff29c17588685145c5ec40afef77014b1eff
mviel <mathieuviel00@gmail.com>
2020-11-08 15:23:04 -0600

    affichage des données sur oled

--- sketch_sep09a.ino
+++ sketch_sep09a.ino
@@ -2,40 +2,35 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
 
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define OLED_RESET     4
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+String temp = "25";
+String heurePompe = "60";
 
 void setup() {
   Serial.begin(9600);
 
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+    for(;;);
   }
 
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
   display.display();
   delay(2000); // Pause for 2 seconds
 
-  // Clear the buffer
   display.clearDisplay();
 
-  // Draw a single pixel in white
   display.drawPixel(10, 10, SSD1306_WHITE);
 
-  // Show the display buffer on the screen. You MUST call display() after
-  // drawing commands to make them visible on screen!
   display.display();
   delay(2000);
 
 
-  testdrawchar();      // Draw characters of the default font
+  testdrawchar(temp, heurePompe);
 
   Serial.println(F("Fin du setup"));
 
@@ -44,7 +39,7 @@ void setup() {
 void loop() {
 }
 
-void testdrawchar(void) {
+void testdrawchar(String temp, String heurePompe) {
   display.clearDisplay();
 
   display.setTextSize(1);      // Normal 1:1 pixel scale
@@ -53,8 +48,8 @@ void testdrawchar(void) {
   display.cp437(true);         // Use full 256 char 'Code Page 437' font
 
   display.write("Adresse ip :\r\n 192.168.0.4 \r\n");
-  display.write("Temperature :\r\n 25 deg \r\n");
-  display.write("Prochaine activation de la pompe :\r\n 2 heures \r\n");
+  display.write("Temperature :\r\n 25" + temp + " \r\n");
+  display.write("Prochaine activation de la pompe :\r\n " + heurePompe + " minustes \r\n");
 
   display.display();
   delay(2000);
