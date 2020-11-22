4f5de3acf4a3296e0266e7ddb3c203c866c7bbd5
mviel <mathieuviel00@gmail.com>
2020-11-22 15:13:17 -0600

    Ajout de l'API

--- sketch_sep09a.ino
+++ sketch_sep09a.ino
@@ -12,6 +12,11 @@
 #define ONE_WIRE_BUS 25
 
 #include <sstream>
+
+#include <ArduinoJson.h>
+StaticJsonDocument<250> jsonDocument;
+char buffer[250];
+
  
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
@@ -30,10 +35,13 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 const int LED = 2;
 
-unsigned int compteur = 0;
-
 String tempVoulu = "25";
-String heurePompe = "360";
+int temp = 0;
+String heurePompe = "600";
+String etatChauff = "";
+
+int compteur15 = 900;
+unsigned int compteur = 0;
 
 const char* PARAM_INPUT_1 = "temp";
 const char* PARAM_INPUT_2 = "heure";
@@ -59,6 +67,8 @@ const char index_html[] PROGMEM = R"rawliteral(
     </style>
 </head>
 <body>
+    <div id="translator_2894"><noscript>Javascript is disable - <a href="http://www.supportduweb.com/">http://www.supportduweb.com/</a> - <a href="http://www.supportduweb.com/generateur-boutons-traduction-translation-google-gratuit-html-code=script-boutons-traduire-page-web.html">Générateur de boutons de traduction</a></noscript></div><script type="text/javascript" src="http://services.supportduweb.com/translator/2894-2-ynnynnnnn.js"></script>    
+
     <div class='w3-card w3-blue w3-padding-small w3-jumbo w3-center'>
         <h1>AQUARIUM CONNECTE</h1>
     </div>
@@ -70,15 +80,15 @@ const char index_html[] PROGMEM = R"rawliteral(
             <p>Etat de la pompe: </p>
             <p>Activer la pompe (15 minutes) tout les 
                 <select class='w3-select' name='heure' id='heure'>
-                    <option value='10'>10</option>
-                    <option value='20'>20</option>
-                    <option value='30'>30</option>
-                    <option value='40'>40</option>
-                    <option value='50'>50</option>
-                    <option value='60'>60</option>
-                    <option value='70'>70</option>
-                    <option value='80'>80</option>
-                    <option value='90'>90</option>
+                    <option value='600'>10</option>
+                    <option value='1200'>20</option>
+                    <option value='1800'>30</option>
+                    <option value='2400'>40</option>
+                    <option value='3000'>50</option>
+                    <option value='3600'>60</option>
+                    <option value='4200'>70</option>
+                    <option value='4800'>80</option>
+                    <option value='5400'>90</option>
                 </select>
              minutes</p>
              <button class='w3-btn w3-blue w3-block'>Appliquer</button>
@@ -92,7 +102,46 @@ void notFound(AsyncWebServerRequest *request) {
   request->send(404, "text/plain", "Not found");
 }
 
-void testdrawchar(float temp, String heurePompe) {
+void create_json(char *tag, String value, char *unit) { 
+  jsonDocument.clear(); 
+  jsonDocument["type"] = tag;
+  jsonDocument["value"] = value;
+  jsonDocument["unit"] = unit;
+  serializeJson(jsonDocument, buffer);
+  Serial.println("Buffer:");
+  Serial.println(buffer);  
+}
+
+void setup_routing() {
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+    Serial.println("Get temperature");
+    create_json("temp", String(temp), "°C");
+    request->send_P(200, "application/json", buffer);
+  });
+  server.on("/tempVoulu", HTTP_GET, [](AsyncWebServerRequest *request){
+    Serial.println("Get temperatureVoulu");
+    create_json("tempVoulu", String(tempVoulu), "°C");
+    request->send_P(200, "application/json", buffer);
+  });
+  server.on("/etatPompe", HTTP_GET, [](AsyncWebServerRequest *request){
+    Serial.println("Get etatPompe");
+    create_json("etatPompe", String(etatChauff), "");
+    request->send_P(200, "application/json", buffer);
+  });
+  server.on("/heurePompe", HTTP_GET, [](AsyncWebServerRequest *request){
+    Serial.println("Get heurePompe");
+    create_json("heurePompe", String(heurePompe), "min");
+    request->send_P(200, "application/json", buffer);
+  });
+  server.on("/restPompe", HTTP_GET, [](AsyncWebServerRequest *request){
+    Serial.println("Get restPompe");
+    create_json("restPompe", String(compteur), "min");
+    request->send_P(200, "application/json", buffer);
+  });
+ 
+}
+
+void testdrawchar(float temp, String heurePompe, String etatChauff, String IpA) {
   display.clearDisplay();
   delay(200);
   display.setTextSize(1);      // Normal 1:1 pixel scale
@@ -100,35 +149,35 @@ void testdrawchar(float temp, String heurePompe) {
   display.setCursor(1, 0);     // Start at top-left corner
   display.cp437(true);         // Use full 256 char 'Code Page 437' font
 
-  display.print("Adresse ip: \r\n 192.168.105.203 \r\n");
-  display.print("Temperature: " + String(temp) + " deg \r\n");
-  display.print("Prochaine activation de la pompe: " + heurePompe + " minutes");
+  display.print("@ip : "+ IpA +" \r\n");
+  display.print("Temperature: " + String(temp) + " C \r\n");
+  display.print("Prochaine activation de la pompe: " + heurePompe + " min \r\n");
+  display.print("Etat du chauffage: " + etatChauff);
   
 
   display.display();
   delay(2000);
 }
 
-//void chauffe(int tempActuel, String tempVoulu){
-//  int value = atoi(tempVoulu.c_str());
-//  if(tempActuel <= value){
-//    digitalWrite(LED, HIGH);
-//  }
-//  else{
-//    digitalWrite(LED, LOW);
-//  }
-//}
+String chauffe(int tempActuel, String tempVoulu){
+  int value = atoi(tempVoulu.c_str());
+  String etat = "";
+  if(tempActuel < value){
+    etat = "En chauffe";
+  }
+  else{
+    etat = "En arret";
+  }
+  return etat;
+}
 
 void activationPompe(int compteur, String heurePompe){
   int value = atoi(heurePompe.c_str());
-  if (compteur < 90){
+  if (compteur < 900){
     digitalWrite(LED, HIGH);
   }
-  else if (compteur > 90) {
+  else if (compteur > 900) {
     digitalWrite(LED, LOW);
-  }
-  else if (compteur < 1){
-    compteur = 90 + value;
   }
 }
 
@@ -157,6 +206,8 @@ void setup()
   server.begin();
 
   Serial.println("Serveur web actif!");
+  Serial.println("WiFi connected with IP: ");
+  Serial.println(WiFi.localIP());
   
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send_P(200, "text/html", index_html);
@@ -167,21 +218,40 @@ void setup()
     request->send_P(200, "text/html", index_html);
   });
 
+
+  sensors.requestTemperatures(); 
+  temp = sensors.getTempCByIndex(0);
+  
+  int pompeInt = atoi(heurePompe.c_str());
+  compteur = compteur15 + pompeInt;
+  Serial.println(compteur);
+
+  setup_routing();  
+
   pinMode(LED, OUTPUT);
 }
 
 void loop(){
+
   sensors.requestTemperatures(); 
   Serial.print("temperature: ");
   Serial.println(sensors.getTempCByIndex(0));
 
-  //chauffe(sensors.getTempCByIndex(0), tempVoulu);
-  testdrawchar(sensors.getTempCByIndex(0), heurePompe);
+  
+  etatChauff = chauffe(sensors.getTempCByIndex(0), tempVoulu);
+  testdrawchar(sensors.getTempCByIndex(0), heurePompe, etatChauff, String(WiFi.localIP()));
+  compteur = compteur - 10;
+  int pompeInt = atoi(heurePompe.c_str());
+  if (compteur < 11){
+    compteur = 900 + pompeInt;
+  }
+  Serial.println(compteur);
   activationPompe(compteur, heurePompe);
   
+
   Serial.println(tempVoulu);
   Serial.println(heurePompe);
 
-  delay(2000);
+  delay(10000);
 
 }
